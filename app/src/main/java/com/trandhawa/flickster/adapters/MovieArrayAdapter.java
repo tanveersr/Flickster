3e75ef8b58d412d8e0cd6a4c35f7121f80f128d5
Tanveer <trandhawa@salesforce.com>
2017-09-17 12:09:14 -0700

    implemented row view recycling

--- app/src/main/java/com/trandhawa/flickster/adapters/MovieArrayAdapter.java
+++ app/src/main/java/com/trandhawa/flickster/adapters/MovieArrayAdapter.java
@@ -1,8 +1,18 @@
 package com.trandhawa.flickster.adapters;
 
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.content.Context;
 import java.util.List;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
+import com.trandhawa.flickster.R;
 import com.trandhawa.flickster.models.Movie;
 
 /**
@@ -13,5 +23,34 @@ public class MovieArrayAdapter extends ArrayAdapter<Movie> {
 
     public MovieArrayAdapter(Context context, List<Movie> movies){
         super(context, android.R.layout.simple_list_item_1, movies);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+
+        // get data item for position
+        Movie movie = getItem(position);
+
+        // check the existing view being used
+        if(convertView == null){
+            LayoutInflater inflater = LayoutInflater.from(getContext());
+            convertView = inflater.inflate(R.layout.item_movie, parent, false);
+        }
+
+        // find the image view
+        ImageView ivImage = (ImageView) convertView.findViewById(R.id.ivMovieImage);
+        // clear out image from converView
+        ivImage.setImageResource(0);
+
+        TextView tvTitle = (TextView) convertView.findViewById(R.id.tvTitle);
+        TextView tvOverview = (TextView) convertView.findViewById(R.id.tvOverview);
+
+        // populate data
+        tvTitle.setText(movie.getOriginalTitle());
+        tvOverview.setText(movie.getOverview());
+
+        Picasso.with(getContext()).load(movie.getPosterPath()).into(ivImage);
+        return convertView;
     }
 }
